7efc9cb475a2a7af4933a3bf7ecfb7838ff02992
ethiclesshacker <mailingadityavs@gmail.com>
2021-04-24 16:08:27 +0530

    Edited to make trackpad toggling more generalised.

--- trackpad-toggle.sh
+++ trackpad-toggle.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-device=17
+device=`xinput list | grep -i "synaptics" | cut -d '=' -f 2 | cut -f 1`
+# device=17
 state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
 
 if [ $state == '1' ];then
